e7fd587e84f23ca06dfeaee14645e10b9aecf0ca
SpiritedCrayon <150854285+SpiritedCrayon@users.noreply.github.com>
2023-11-14 20:47:01 +0000

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      document.getElementById("loadingScreen").style.display = "none";
+      document.getElementById("content").style.display = "block";
+    }, 2000);
+  });
+  
